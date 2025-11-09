ea4132e4f7ad978cde69b92273a16a4caea01f2f
raneem49 <apoflijaraneem@gamil.com>
2025-11-09 17:38:37 +0200

    تحديث الموقع

--- .history/main/models_20251109173702.py
+++ .history/main/models_20251109173702.py
@@ -16,8 +16,7 @@ class Patient(models.Model):
     phone = models.CharField(max_length=15)
     disease = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    notes = models.TextField(blank=True, null=True) 
-    
+     notes = models.TextField(blank=True, null=True) 
     def __str__(self):
         return self.name
 

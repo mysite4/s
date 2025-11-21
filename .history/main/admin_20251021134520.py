d9c3a21eda9492f2a85231699f09f04d44daa95e
raneem49 <apoflijaraneem@gamil.com>
2025-11-21 13:43:05 +0200

    تحديث الموقع

--- .history/main/admin_20251021134520.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Doctor, Appointment
-
-admin.site.register(Doctor)
-admin.site.register(Appointment)
--- .history/main/views_20251021133900.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html')
-from django.shortcuts import render
-
-def booking_page(request):
-    return render(request, 'booking.html')
